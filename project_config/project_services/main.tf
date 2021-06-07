abda267cf0e80bc4e4e846f3c0fa84653cacd51d
Nishant Nasa <13643243+nishantnasa@users.noreply.github.com>
2021-06-08 01:04:01 +1000

    add default api services into the module

--- project_config/project_services/main.tf
+++ project_config/project_services/main.tf
@@ -11,10 +11,44 @@ locals {
   api_set = var.enable_apis ? toset(distinct(flatten(jsondecode(var.activate_apis)))) : []
 }
 
+resource "google_project_service" "service_usage" {
+  project                    = var.project_id
+  service                    = "serviceusage.googleapis.com"
+  disable_on_destroy         = false
+  disable_dependent_services = false
+}
+
+
+resource "google_project_service" "cloud_resource_manager" {
+  project                    = var.project_id
+  service                    = "cloudresourcemanager.googleapis.com"
+  disable_on_destroy         = false
+  disable_dependent_services = false
+
+  depends_on = [
+    google_project_service.service_usage
+  ]
+}
+
+resource "google_project_service" "iam" {
+  project                    = var.project_id
+  service                    = "iam.googleapis.com"
+  disable_on_destroy         = false
+  disable_dependent_services = false
+
+  depends_on = [
+    google_project_service.cloud_resource_manager
+  ]
+}
+
 resource "google_project_service" "project_services" {
   for_each                   = local.api_set
   project                    = var.project_id
   service                    = each.value
   disable_on_destroy         = var.disable_services_on_destroy
   disable_dependent_services = var.disable_dependent_services
+
+  depends_on = [
+    google_project_service.iam
+  ]
 }
